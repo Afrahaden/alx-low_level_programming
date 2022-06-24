00c4dd73866a84be9a83bfd8bcaa9949ff12ed31
Afrah Aden <afrahaden05@gmail.com>
2022-06-24 04:26:59 -0700

    C programming

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[] __attribute__((unused)));
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+
+#endif
